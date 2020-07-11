025f405171b5d3ea4e1c054d0bf1b4bb592cbaa9
OAO <anton.troskin@gmail.com>
2020-07-11 10:39:39 +0300

    mylistas

---
+++ exept.py
@@ -0,0 +1,14 @@
+#
+# # raise Exception("naujas erroras")
+#
+# try:
+#     # raise Exception("ggg")
+#     my_list = []
+#     print("kazkas")
+#
+# except:
+#     print("kitkas")
+
+my_l = ["pirmas"]
+
+print(my_l[0])
